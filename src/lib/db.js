c5982b88005c74de9ac18d4a68eddec0e53f8e2d
Eslam-Shaheer <eslamshaheer@gmail.com>
2024-09-17 15:48:42 +0300

    Fix build

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,9 +1,7 @@
-import { MongoClient } from "mongodb";
-
 let db;
-if (!db) {
-  const mongoClient = new MongoClient("mongodb://localhost:27017/local");
-  const clientPromise = await mongoClient.connect();
-  db = clientPromise.db();
-}
+// if (!db) {
+//   const mongoClient = new MongoClient("mongodb://localhost:27017/local");
+//   const clientPromise = await mongoClient.connect();
+//   db = clientPromise.db();
+// }
 export default db;
