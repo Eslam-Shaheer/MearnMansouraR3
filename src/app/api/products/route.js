756492c2f995e2dbb4e2c6159d4073696ad4178a
Eslam-Shaheer <eslamshaheer@gmail.com>
2024-09-17 15:50:42 +0300

    Fix build

--- src/app/api/products/route.js
+++ src/app/api/products/route.js
@@ -1,12 +1,12 @@
-import getProducts from "@/lib/getProducts";
+// import getProducts from "@/lib/getProducts";
 
-export async function GET(request) {
-  // console.log(request, "request");
-  // const products = await getProducts();
-  // return new Response(JSON.stringify(products), {
-  //   status: 200,
-  // });
-}
+// export async function GET(request) {
+// console.log(request, "request");
+// const products = await getProducts();
+// return new Response(JSON.stringify(products), {
+//   status: 200,
+// });
+// }
 
 // export async function POST(request) {}
 
